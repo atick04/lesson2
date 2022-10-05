139c9441d25644132b3f4822abead4e13ef5d312
atick04 <aitmatov2005@gmail.com>
2022-10-05 19:49:56 +0600

    lesson2.js

---
+++ lesson2.js
@@ -0,0 +1,13 @@
+// обьекты
+let user = {
+  name: "John",
+  age: "27",
+  adress: "new York",
+};
+console.log(user);
+
+// массивы
+let phones = ["iphone", "android", "ios", "samsung", "Redmi"];
+console.log(phones);
+
+typeof Number;
